91e22ecfc1502c5768c5d76cd541f22045dbffd3
gobinda1710 <das23105101193diu.edu.bd>
2024-12-03 21:54:06 +0600

    change

--- build/classes/EmployeeManagement.rs
+++
@@ -1,4 +0,0 @@
-Employee
-EmployeeManagement1
-SalaryManagementSystem
-EmployeeManagement
--- src/EmployeeManagement.java
+++ src/EmployeeManagement.java
@@ -133,6 +133,6 @@ public class EmployeeManagement {
             }
         }
 
-        scanner.close();
+
     }
 }
